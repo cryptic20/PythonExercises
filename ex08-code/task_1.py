735da21e25a5bfacf533d3576e3256a9df83f8e7
cryptic20 <sherwin.wyco20@gmail.com>
2018-11-18 14:40:50 -0500

    finished task 1

--- ex08-code/task_1.py
+++ ex08-code/task_1.py
@@ -3,52 +3,45 @@ class Aircraft:
         self._number_of_passenger = number_of_passenger
         self._name = model
 
-    @property
     def get_number_of_passengers(self):
         return self._number_of_passenger
 
-    @get_number_of_passengers.setter
-    def get_number_of_passengers(self, number_of_passenger):
-        self._number_of_passenger = number_of_passenger
-
-    @property
     def get_name(self):
         return self._name
 
-    @get_name.setter
-    def get_name(self, model):
-        self._name = model
-
 
 class IntercontinentalAircraft(Aircraft):
-    def __int__(self, number_of_passenger, model, cargo_hold):
+    def __init__(self, number_of_passenger, model, cargo_hold):
         Aircraft.__init__(self, number_of_passenger, model)
         self.__cargo_hold = cargo_hold
 
     def calculate_amount_of_fuel(self, km):
-        return (.25 * km * self.get_number_of_passengers) + (2 * self.__cargo_hold)
+        return (.25 * km * self.get_number_of_passengers()) + (2 * km * self.__cargo_hold)
 
+    @property
     def manifest(self):
-        return 'IntercontinentalAircraft: Intercontinental flight {0}: passenger count {1}, ' \
-               'cargo load {2}'.format(self.get_name, self.get_number_of_passengers, self.__cargo_hold)
+        return 'Intercontinental flight {0}: passenger count {1}, cargo load {2}'\
+            .format(self.get_name(), self.get_number_of_passengers(), self.__cargo_hold)
 
 
 class ShortHaulAircraft(Aircraft):
-    def __int__(self, number_of_passenger, model):
+    serial_number = -1
+
+    def __init__(self, number_of_passenger, model):
         Aircraft.__init__(self, number_of_passenger, model)
-        self.serial_number = 0
+        ShortHaulAircraft.serial_number += 1
+        self.__serial_number = ShortHaulAircraft.serial_number
 
     def get_serial_number(self):
-        get_old_serial = self.serial_number
-        self.serial_number += 1  # increment
-        return get_old_serial
+        return self.__serial_number
 
     def calculate_amount_of_fuel(self, km):
-        return .1 * km * self.get_number_of_passengers
+        return .1 * km * self.get_number_of_passengers()
 
+    @property
     def manifest(self):
         return 'Short haul flight serial number {0}, name {1}: passenger count {2}'\
-            .format(self.get_serial_number, self.get_name, self.get_number_of_passengers)
+            .format(self.get_serial_number(), self.get_name(), self.get_number_of_passengers())
 
 
 class ControlTower:
@@ -58,7 +51,7 @@ class ControlTower:
     def add_aircraft(self, plane):
         self.aircraft_hangar.append(plane)
 
-    def get_manifest(self):
+    def get_manifests(self):
         return [ac.manifest for ac in self.aircraft_hangar]
 
 
@@ -66,6 +59,8 @@ if __name__ == '__main__':
     intercontinental_flight = IntercontinentalAircraft(500, "Boeing-747", 100)
     short_haul_flight = ShortHaulAircraft(110, "Airbus-A220")
     short_haul_flight2 = ShortHaulAircraft(85, "Airbus-A220")
+
+    print(short_haul_flight.get_serial_number())
 
     assert short_haul_flight.get_serial_number() == 0
     assert short_haul_flight2.get_serial_number() == 1
