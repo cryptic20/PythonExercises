100ada98a349a4ab876d38996444faa996a826a0
cryptic20 <sherwin.wyco20@gmail.com>
2018-11-26 22:43:49 -0500

    complete listing 1

--- ex09-code/task_1.py
+++ ex09-code/task_1.py
@@ -1,43 +1,56 @@
 class CarWash(object):
-    job_counter = 0
-
     def __init__(self):
         self.persistence = {}
         self.sms_sender = SmsSender()
 
     def register_car_for_wash(self, car, customer):
-        job_id = CarWash.new_job_id()
-        self.persistence[job_id] = (car, customer)
-        return job_id
+        job = CarWashJob(car, customer)
+        self.persistence[job.job_id] = job
+        return job.job_id
 
     def complete_wash(self, job_id):
-        car, customer = self.persistence[job_id]
-        phone_number = customer.mobile_phone
-        msg = f'Job {job_id}, Car {car.plate} washed.'
-        self.sms_sender.send(phone_number, msg)
+        job = self.persistence[job_id]
+        self.sms_sender.send(job.contact_details, job.notification_message)
+
+
+class CarWashJob:
+    job_counter = 0
+
+    def __init__(self, car, customer, *job_id):
+        self.car = car
+        self.customer = customer
+        if job_id:
+            self.job_id = job_id
+        else:
+            self.job_id = self.new_job_id()
 
     @staticmethod
     def new_job_id():
-        job_id = CarWash.job_counter
-        CarWash.job_counter += 1
+        job_id = CarWashJob.job_counter
+        CarWashJob.job_counter += 1
         return f'#{job_id}'
+
+    @property
+    def contact_details(self):
+        return self.customer.mobile_phone
+
+    @property
+    def notification_message(self):
+        return f'Job {self.job_id}, Car {self.car.plate}'
 
 
 class Customer(object):
-
     def __init__(self, name, mobile_phone):
         self.name = name
         self.mobile_phone = mobile_phone
 
 
 class Car(object):
-
     def __init__(self, plate):
         self.plate = plate
 
 
 class SmsSender(object):
-
     def send(self, phone_number, msg):
         print(f'Sending text message to {phone_number}: {msg}')
         # ... do some weird SMS magic
@@ -55,3 +68,4 @@ if __name__ == '__main__':
     assert job_id1 != job_id2
 
     car_wash.complete_wash(job_id1)
+
